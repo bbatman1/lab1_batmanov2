be2903206a36e08e7e462045fa6e976722b5ee04
bbatman1 <egor.batmanov123@gmail.com>
2024-10-28 21:54:45 +0300

    забыл сделать коммит

--- batman_lab1/batman_lab1.cpp
+++ batman_lab1/batman_lab1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>  
+﻿#include <iostream>
 #include <fstream>  
 #include <string> 
 
@@ -23,9 +23,9 @@ int check() {
     int num;
     while (true) {
         cin >> num;
-        if (cin.fail() || num <= 0 || cin.peek() != '\n') {
+        if (cin.fail() || num <= 0 || cin.peek() != '\n') { // cin.fail() возвращает true, если нашлась ошибка при вводе; cin.peek() spaces
             cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); //  Пропускает все оставшиеся символы в буфере ввода до символа новой строки
             cout << "Ошибка. Попробуйте снова: ";
         }
         else {
@@ -60,11 +60,6 @@ tube tube_cin() {
     T.MM = check();
     T.CHECK = true;
     return T;
-};
-
-void switch_status(tube& T) {
-    T.CHECK =!T.CHECK;
-    cout << "Статус изменен!" << endl;
 }
 
 KS KS_cin() {
@@ -79,30 +74,46 @@ KS KS_cin() {
     while (true) {
         if (K.KCV > K.KC) {
             cout << "Количество цехов в работе не может быть больше чем всего цехов" << endl;
-            cin >> K.KCV;
+            K.KCV = check();
         }
         else {
             break;
         }
-    };
+    }
     cout << "эффективность " << endl;
     K.EFFECT = checkfloat();
-    return K;
-};
+    while (true) {
+        if (K.EFFECT > 1) {
+            cout << "Эффективность может быть от 0 до 1" << endl;
+            K.EFFECT = checkfloat();
+        }
+        else {
+            break;
+        }
+
+        return K;
+    }
+}
 
 void show(const tube& T) {
     cout << "название трубы " << T.Name << endl;
     cout << "длина " << T.KM << endl;
     cout << "диаметр " << T.MM << endl;
-    cout << "в ремонте ли? " << T.CHECK << endl;
+    if (T.CHECK) {
+        cout << "Труба в рабочем состоянии" << endl;
+    }
+    else {
+        cout << "Труба не работает" << endl;
+    }
 
-};
+}
+
 void show_2(const KS& K) {
     cout << "название КС " << K.Name1 << endl;
     cout << "количество цехов " << K.KC << endl;
     cout << "количество цехов в работе " << K.KCV << endl;
     cout << "эффективность " << K.EFFECT << endl;
-};
+}
 
 void view_all(const tube& T, const KS& K) {
     if (K.KC > 0 && T.MM > 0) {
@@ -122,62 +133,82 @@ void view_all(const tube& T, const KS& K) {
     }
 }
 
-void edittubs(tube T) {
-    if (T.CHECK == 0 or T.CHECK == false) {
-        T.CHECK = true;
-        cout << "Труба в рабочем состоянии" << endl;
-        if (T.CHECK == 1 or T.CHECK == true) {
-            T.CHECK = false;
-            cout << "Труба в теперь в ремонте" << endl;
-        }
-    }
+void switch_status(tube& T) {
+    T.CHECK = !T.CHECK;
+    cout << "Статус изменен!" << endl;
 }
 
+void StartKC(KS& K) {
+    cout << "Всего " << K.KC << " в работе " << K.KCV << endl;
+    int count;
+    int delta;
+    delta = K.KC - K.KCV; // сколько ещё можно запустить
 
-void editKS(KS K) {
+    if (delta == 0)
+    {
+        cout << "Все станции итак работают" << endl;
+        return;
+    }
 
-    string boolean_const = "";
-    cout << "количество цехов" << K.KC << endl;
-    cout << "количество цехов в работе" << K.KCV << endl;
-    cout << "Введите 1 для запуска цеха и 0 для его остановки" << endl;
-    cin >> boolean_const;
-    if (K.KCV == 0 and boolean_const == "0") {
-        cout << "Все цеха и так выключены" << endl;
-        cout << "Включить цех? Введите 1,если да" << endl;
-        cin >> boolean_const;
-    }
-    if (boolean_const != "0" and boolean_const != "1") {
-        cout << "Вы ввели не 1 и 0. Введите 1 для запуска цеха и 0 для его остановки";
-        cin >> boolean_const;
-    }
-    if (boolean_const == "0") {
-        K.KCV = K.KCV - 1;
-    }
-    if (boolean_const == "1") {
-        K.KCV = K.KCV + 1;
-    }
-    cout << "количество цехов в работе" << K.KCV << endl;
+    do {
+        cout << " возможно запустить" << delta << "цехов: ";
+        count = check();
+    } while (count < 1 || count > delta);
+
+    K.KCV += count;
+    cout << count << "теперь работает столько станций" << K.KCV << endl;
 }
 
-void save_to(tube T,KS K) {
+void StopKC(KS& K) {
+    cout << "Всего " << K.KC << " в работе " << K.KCV << endl;
+    int count;
 
+    if (K.KCV == 0)
+    {
+        cout << "Работающих станций нет" << endl;
+        return;
+    }
+    do {
+        cout << "Введите сколько станций сделать рабомичи " ;
+        count = check();
+    } while (count < 1 || count > K.KCV);
+
+    K.KCV -= count;
+    cout << count << "теперь работают столько станций" << K.KCV << endl;
+}
+
+void save1(const tube& T, ofstream& out)
+{
+    out << "data Pipe:" << endl;
+    out << T.Name << endl;
+    out << T.KM << " " << T.MM << " " << T.CHECK << endl;
+}
+
+void save2(const KS& K, ofstream& out)
+{
+    out << "data Station:" << endl;
+    out << K.Name1 << endl;
+    out << K.KC << " " << K.KCV << " " << K.EFFECT << endl;
+}
+
+void saveall(const tube& T, const KS& K) {
+    if (T.Name.empty() && K.Name1.empty()) {
+        cout << "Имен нет!" << endl;
+        return;
+    }
     ofstream out;
-    out.open("zapis_in_file.txt");
+    out.open("zapis_in_file");
     if (out.is_open())
     {
-        string Data = "";
-        Data = "Tube" + '\n' + T.Name + '\n';
-        Data = Data + to_string(T.KM) + '\n';
-        Data = Data + to_string(T.MM) + '\n';
-        Data = Data + to_string(T.CHECK) + '\n';
-        string Data2 = "";
-        Data2 = "KS" + '\n' + K.Name1 + '\n';
-        Data2 = Data2 + to_string(K.KC) + '\n';
-        Data2 = Data2 + to_string(K.KCV) + '\n';
-        Data2 = Data2 + to_string(K.EFFECT) + '\n';
-        out << Data2 << std::endl;
+        if (!T.Name.empty()) {
+            save1(T, out);
+            cout << "Труба сохранена!" << endl;
+        }
+        if (!K.Name1.empty()) {
+            save2(K, out);
+            cout << "КС сохранена!" << endl;
+        }
 
-        out << Data << endl;
     }
     out.close();
 }
@@ -213,40 +244,49 @@ void menu() {
     tube T;
     KS K;
     while (true) {
-            cout << "1. Добавить трубу" << endl;
-            cout << "2. Добавить КС" << endl;
-            cout << "3. Просмотр всех объектов" << endl;
-            cout << "4. Редактировать трубу" << endl;
-            cout << "5. Редактировать КС" << endl;
-            cout << "6. Сохранить данные " << endl;
-            cout << "7. Загрузить данные " << endl;
-            cout << "0. Выход" << endl;
-            int number;
-            cin >> number;
-            switch (number) {
-            case 1:
-                T = tube_cin();
+        cout << "1. Добавить трубу" << endl;
+        cout << "2. Добавить КС" << endl;
+        cout << "3. Просмотр всех объектов" << endl;
+        cout << "4. Редактировать трубу" << endl;
+        cout << "5. Редактировать КС" << endl;
+        cout << "6. Сохранить данные " << endl;
+        cout << "7. Загрузить данные " << endl;
+        cout << "8. Выход" << endl;
+        int number;
+        cin >> number;
+        switch (number) {
+        case 1:
+            T = tube_cin();
+            break;
+        case 2:
+            K = KS_cin();
+            break;
+        case 3:
+            view_all(T, K);
+            break;
+        case 4: switch_status(T);
+            break;
+        case 5: 
+            cout << "1. Включить цех(и)\n2. Выключить цех(и)." << endl;
+            int number_choose;
+            cin >> number_choose;
+            switch (number_choose) {
+            case 1: StartKC(K);
                 break;
-            case 2:
-                K = KS_cin();
+            case 2: StopKC(K);
                 break;
-            case 3:
-                view_all(T,K);
-                break;
-            case 4: edittubs(T);
-                break;
-            case 5: editKS(K);
-                break;
-            case 6: save_to(T,K);
-                break;
-            case 7: loads_from_file(T);
-                break;
-            case 0: 
-                break;
-            default: cout << "Данная команда отсутствует\n";
-
-
             }
+            break;
+        case 6: saveall(T, K);
+            break;
+        case 7: loads_from_file(T, K);
+            break;
+        case 8:
+            break;
+        default: cout << "Данная команда отсутствует\n";
+
+
+        }
 
 
     }
