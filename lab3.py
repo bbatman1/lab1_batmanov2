83f5961a046900e73e3ef5ecd845e09c90ea6e22
bbatman1 <egor.batmanov123@gmail.com>
2024-12-26 13:30:19 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 
 filess = []
 
@@ -9,6 +9,96 @@ for root, dirs, files in os.walk(r"C:\Users\HP\Downloads\Telegram Desktop"):
         if file.endswith('Dado.txt'):
             print(file)
             filess.append(file)
+            
+
+class Graph_max:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
+    def add_edge(self, u, v, w):
+        self.graph[u][v] = w
+
+    def bfs(self, s, t, parent):
+        visited = [False] * self.V
+        queue = []
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            u = queue.pop(0)
+
+            for v in range(self.V):
+                if visited[v] == False and self.graph[u][v] > 0:
+                    queue.append(v)
+                    visited[v] = True
+                    parent[v] = u
+
+        return True if visited[t] else False
+
+    def ford_fulkerson(self, source, sink):
+        parent = [-1] * self.V
+        max_flow = 0
+
+        while self.bfs(source, sink, parent):
+            path_flow = float('Inf')
+            s = sink
+
+            while s != source:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+
+            max_flow += path_flow
+
+            v = sink
+            while v != source:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
+
+
+            
+
+            
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+        
+    def construct_graph(self, nodes, init_graph):
+        '''
+        Этот метод обеспечивает симметричность графика. Другими словами, если существует путь от узла A к B со значением V, должен быть путь от узла B к узлу A со значением V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+        
+        graph.update(init_graph)
+        
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+                    
+        return graph
+    
+    def get_nodes(self):
+        "Возвращает узлы графа"
+        return self.nodes
+    
+    def get_outgoing_edges(self, node):
+        "Возвращает соседей узла"
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+    
+    def value(self, node1, node2):
+        "Возвращает значение ребра между двумя узлами."
+        return self.graph[node1][node2]
 class Counter:
     id = 0
     def __new__(cls, *args, **kwargs):
@@ -370,9 +460,11 @@ def load():
             elif 'КС\n' == m[i]:
                 kss.append(KS(m[i+2], int(m[i+3]), int(m[i+4]), float(m[i+5])))
                 kss[-1].id = int(m[i+1])
+            
 
     return trubaa, kss
-
+jetwork = {}
+inits = []
 def join_KS(truba, ks):
     if ks and truba:
         m = input("Введите через пробел ID КС входа, ID КС выходы и ID свободной трубы: ")
@@ -420,6 +512,11 @@ def join_KS(truba, ks):
             flag = False
         flag1,flag2 = False,False
         for i in ks:
+            if (i.id == idis[0] or i.id==idis[1]) and jetwork.get(i.id):
+                continue
+            elif (i.id == idis[0] or i.id==idis[1]) and not(jetwork.get(i.id)):
+                jetwork[i.id] = {}
+        for i in ks:
             if i.id == idis[0]:
                 flag1 = True
             if i.id == idis[1]:
@@ -432,6 +529,8 @@ def join_KS(truba, ks):
                         i.is_empty = True
                         network[idis[0]] = [idis[1]]
                         network[idis[1]]=[]
+                        jetwork[idis[0]][idis[1]] = i.km
+                        inits.append([idis[0]-1,idis[1]-1, i.mm])
                         flag = True
                         break
             if not(flag):
@@ -439,7 +538,10 @@ def join_KS(truba, ks):
                 truba.append(add_t())
                 truba[-1].is_empty = True
                 network[idis[0]] = [idis[1]]
-                network[idis[1]]=[]
+                network[idis[1]]=[]                
+                jetwork[idis[0]][idis[1]] = truba[-1].km
+                inits.append([idis[0]-1,idis[1]-1, truba[-1].mm])
+
         else:
             print("КС с такими ID не существует")
     else:
@@ -460,22 +562,81 @@ def topological_sort_dfs(graph):
     print(f"Отсортированный граф: {stack[::-1]}")
     return stack[::-1]  
 
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
+ 
+    shortest_path = {}
+ 
+    previous_nodes = {}
+ 
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # Однако мы инициализируем значение начального узла 0  
+    shortest_path[start_node] = 0
+    
+    # Алгоритм выполняется до тех пор, пока мы не посетим все узлы
+    while unvisited_nodes:
+        # Приведенный ниже блок кода находит узел с наименьшей оценкой
+        current_min_node = None
+        for node in unvisited_nodes: # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+                
+        # Приведенный ниже блок кода извлекает соседей текущего узла и обновляет их расстояния
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+ 
+        # После посещения его соседей мы отмечаем узел как "посещенный"
+        unvisited_nodes.remove(current_min_node)
+    
+    return previous_nodes, shortest_path
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+    
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+ 
+   # Добавить начальный узел вручную
+    path.append(start_node)
+    path = list(map(str, path))
+    
+    print("Найден следующий лучший маршрут с ценностью {}.".format(shortest_path[target_node]))
+    print(" -> ".join(reversed(path)))
+def floydat(ks, inits):
+    g = Graph_max(len(ks)+1)
+    
+    for i in inits:
+        g.add_edge(i[0], i[1], i[2])
+    print("Максимальный поток:", g.ford_fulkerson(3, 0))            
+
 
 truba = []
 ks = []
 network = {}
+ups = []
 
 while True:
-    to_do = input("1.Добавить трубу\n2.Добавить КС\n3.Просмотр всех объектов\n4.Редактировать трубу\n5.Редактировать КС\n6.Сохранить\n7.Загрузить\n8.Поиск\n9.Соединить КС трубой\n10.Сортировка\n0.Выход\n")
+    to_do = input("1.Добавить трубу\n2.Добавить КС\n3.Просмотр всех объектов\n4.Редактировать трубу\n5.Редактировать КС\n6.Сохранить\n7.Загрузить\n8.Поиск\n9.Соединить КС трубой\n10.Сортировка\n11.Кратчайший путь\n12.Макс поток\n0.Выход\n")
     if to_do == '0':
         break
-    elif to_do not in list('0123456789')+['10']:
+    elif to_do not in list('0123456789')+['10','11', '12']:
         continue
     elif to_do == "1":
         truba.append(add_t())
 
     elif to_do == "2":
         ks.append(add_k())
+        ups.append(ks[-1].id)
     elif to_do == "3":
         print(all_obj(truba, ks))
     elif to_do == '4':
@@ -492,3 +653,51 @@ while True:
         join_KS(truba, ks)
     elif to_do == '10':
         topological_sort_dfs(network)
+    elif to_do == '12':
+        floydat(ks, inits)
+    elif to_do == "11":
+        m = input("Введите ID КС входа и выхода ЧЕРЕЗ ПРОБЕЛ!!! ").split()
+        while len(m) != 2:
+            m = input("Введите ID КС входа и выхода ЧЕРЕЗ ПРОБЕЛ!!! ").split()
+        try:
+            idis = [int(m[0]), int(m[1])]
+            flag123 = 0
+            flag321 = 0
+            for i in ks:
+                if str(i.id) == str(idis[0]):
+                    flag123 = 1
+                elif str(i.id) == str(idis[1]):
+                    flag321 = 1
+            print(flag123, flag321)
+            if flag123 and flag321:
+                jetwork = Graph(ups, jetwork)
+                r = dijkstra_algorithm(jetwork, idis[0])
+                print_result(r[0], r[1], idis[0], idis[1])
+            else:
+                print("КС с такими ID не существует")
+                m = input("Введите ID КС входа и выхода ЧЕРЕЗ ПРОБЕЛ!!! ").split()
+
+        except:
+            m = input("Введите ID КС входа и выхода через пробел целочисленные значения ").split()
+            while True:
+                try:
+                    idis = [int(m[0]), int(m[1])]
+                    flag123 = 0
+                    flag321 = 0
+                    for i in ks:
+                        if i.id == idis[0]:
+                            flag123 = 1
+                        elif i.id == idis[1]:
+                            flag321 = 1
+                    if flag123 and flag321:
+                        jetwork = Graph(ups, jetwork)
+                        r = dijkstra_algorithm(jetwork, idis[0])
+                        print_result(r[0], r[1], idis[0], idis[1])
+                        break
+                    else:
+                        print("КС с такими ID не существует")
+                        m = input("Введите ID КС входа и выхода ЧЕРЕЗ ПРОБЕЛ!!! ").split()
+                except:
+                    continue
+        
+
